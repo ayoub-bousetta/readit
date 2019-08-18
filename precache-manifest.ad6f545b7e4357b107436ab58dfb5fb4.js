5900262a6b5f9d43a8ccf057b9309bb1124c4820
Aubbusta <aubbusta@gmail.com>
2019-08-18 04:11:39 +0100

    Updates

---
+++ precache-manifest.ad6f545b7e4357b107436ab58dfb5fb4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17daaa649aa0cdc3729b0f8154960705",
+    "url": "/readit/index.html"
+  },
+  {
+    "revision": "bbfca322bc3724e2565f",
+    "url": "/readit/static/css/main.5afa2ee8.chunk.css"
+  },
+  {
+    "revision": "df81114f5ce6c50602e1",
+    "url": "/readit/static/js/2.d7b1431e.chunk.js"
+  },
+  {
+    "revision": "bbfca322bc3724e2565f",
+    "url": "/readit/static/js/main.7c1269bb.chunk.js"
+  },
+  {
+    "revision": "85e9ccc09bce0186bdd5",
+    "url": "/readit/static/js/runtime~main.04c38dcc.js"
+  },
+  {
+    "revision": "bb20d3cd483ccdfb965088c49b5739d8",
+    "url": "/readit/static/media/logo.bb20d3cd.png"
+  }
+]);
